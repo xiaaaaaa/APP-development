c99d73ca4e3fcaf824bfca9d3b163e959d57949b
xiaaaaaa <78157957+xiaaaaaa@users.noreply.github.com>
2023-11-02 22:59:42 +0800

    Add files via upload

---
+++ Stray Animal Detective Agency/js/A_script.js
@@ -0,0 +1,13 @@
+$(window).mousemove(function (e) {
+    $(".circle").css({ top: e.pageY, left: e.pageX }); 
+  }); 
+
+function password(pwd){
+  if(pwd==="9925"){
+    location.href="https://slionkyiu1129.github.io/midterm/D.html";//要改成黑吉自己的github連接喔!
+    alert("你已破案");
+  }
+  else{
+    alert("ACCESS DENIED!");
+  }
+}
