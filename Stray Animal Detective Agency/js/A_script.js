78cddd7e6aad44699a812e2dc89d0c426aab7b01
xiaaaaaa <78157957+xiaaaaaa@users.noreply.github.com>
2023-11-02 23:04:18 +0800

    Update A_script.js

--- Stray Animal Detective Agency/js/A_script.js
+++ Stray Animal Detective Agency/js/A_script.js
@@ -4,7 +4,7 @@ $(window).mousemove(function (e) {
 
 function password(pwd){
   if(pwd==="9925"){
-    location.href="https://slionkyiu1129.github.io/midterm/D.html";//要改成黑吉自己的github連接喔!
+    location.href="https://xiaaaaaa.github.io/F2E_Front-End_Engineer_Design/Stray%20Animal%20Detective%20Agency/D.html";//要改成黑吉自己的github連接喔!
     alert("你已破案");
   }
   else{
