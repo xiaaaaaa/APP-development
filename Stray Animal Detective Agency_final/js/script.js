fef86bcefb1824f962f4fc8f03e0ab01c71c1b9b
Slionkyiu1129 <130773257+Slionkyiu1129@users.noreply.github.com>
2023-12-29 00:10:18 +0800

    Ａ頁完成、已修正Ｈ頁陰影

--- Stray Animal Detective Agency_final/js/script.js
+++ Stray Animal Detective Agency_final/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Stray Animal Detective Agency_final/final/js/script.js
 $(window).mousemove(function (e) {
     $(".circle").css({ top: e.pageY, left: e.pageX }); 
   }); 
@@ -15,35 +14,3 @@ function password(pwd){
   }
   }
 
-  window.addEventListener('scroll', () => {
-    const scrollPositionY = window.pageYOffset;
-    const layer0 = document.querySelector('.keyart-layer0');
-    const layer1 = document.querySelectorAll('.keyart-layer1');
-    const layer2 = document.querySelectorAll('.keyart-layer2');
-    //const parallaxScrollingElements = [...stars, moon];
-    const parallaxScrollingElements = [keyart-layer0, keyart-layer1, keyart-layer2];
-  
-    parallaxScrollingElements.forEach((element) => {
-      const elementMoveSpeed = Number(element.dataset.speed);
-      element.style.transform = `
-        translateY(${scrollPositionY * elementMoveSpeed}px)
-      `;
-    });
-  });
-=======
-$(window).mousemove(function (e) {
-    $(".circle").css({ top: e.pageY, left: e.pageX }); 
-  }); 
-
-function password(pwd){
-  if(pwd==="9925"){
-    location.href="https://www.youtube.com/";
-    //location.href="./index1.html";
-    //location.href="https://slionkyiu1129.github.io/midterm/D.html";//要改成黑吉自己的github連接喔!
-    alert("你已破案");
-  }
-  else{
-    alert("ACCESS DENIED!");
-  }
-  }
->>>>>>> 5b118f54751606011dc7493084e185cd605bfb3d:Stray Animal Detective Agency_final/js/script.js
