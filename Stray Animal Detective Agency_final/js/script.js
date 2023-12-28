1864368875e3a263dfd1859c42d68a418f9a3008
Slionkyiu1129 <130773257+Slionkyiu1129@users.noreply.github.com>
2023-12-29 07:56:35 +0800

    9925結案快捷

--- Stray Animal Detective Agency_final/js/script.js
+++ Stray Animal Detective Agency_final/js/script.js
@@ -4,9 +4,9 @@ $(window).mousemove(function (e) {
 
 function password(pwd){
   if(pwd==="9925"){
-    location.href="https://www.youtube.com/";
+    //location.href="https://www.youtube.com/";
     //location.href="./index1.html";
-    //location.href="https://slionkyiu1129.github.io/midterm/D.html";//要改成黑吉自己的github連接喔!
+    location.href="https://xiaaaaaa.github.io/F2E_Front-End_Engineer_Design/Stray%20Animal%20Detective%20Agency_final/D.html";//要改成黑吉自己的github連接喔!
     alert("你已破案");
   }
   else{
