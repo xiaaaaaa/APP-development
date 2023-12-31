d905e80d0eb560e9f05dfb94a8ffce47de661bd9
Slionkyiu1129 <130773257+Slionkyiu1129@users.noreply.github.com>
2023-12-31 12:41:37 +0800

    Recover script.js

--- Stray Animal Detective Agency_final/js/script.js
+++ Stray Animal Detective Agency_final/js/script.js
@@ -1,9 +1,9 @@
 
 function password(pwd){
   if(pwd==="9925"){
-    location.href="https://www.youtube.com/";
+    //location.href="https://www.youtube.com/";
     //location.href="./index1.html";
-    //location.href="https://xiaaaaaa.github.io/F2E_Front-End_Engineer_Design/Stray%20Animal%20Detective%20Agency_final/D.html";//要改成黑吉自己的github連接喔!
+    location.href="https://xiaaaaaa.github.io/F2E_Front-End_Engineer_Design/Stray%20Animal%20Detective%20Agency_final/D.html";//要改成黑吉自己的github連接喔!
     alert("你已破案");
   }
   else{
